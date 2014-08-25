7b94b2b2b9e931079dbd3c01445ac034d1333aef
andre <ferreira.andreluiz@gmail.com>
2014-08-25 17:07:43 -0300

    builder

---
+++ Builder2/src/VolksBuilder.java
@@ -0,0 +1,34 @@
+
+public class VolksBuilder extends CarroBuilder {
+
+	 @Override
+	    public void buildPreco() {
+	        // Operação complexa. 
+	        carro.preco = 35000.00;
+	    }
+	 
+	    @Override
+	    public void buildDscMotor() {
+	        // Operação complexa.
+	        carro.dscMotor = "Gol 1.0";
+	    }
+	 
+	    @Override
+	    public void buildAnoDeFabricacao() {
+	        // Operação complexa.
+	        carro.anoDeFabricacao = 2014;
+	    }
+	 
+	    @Override
+	    public void buildModelo() {
+	        // Operação complexa.
+	        carro.modelo = "Gol";
+	    }
+	 
+	    @Override
+	    public void buildMontadora() {
+	        // Operação complexa.
+	        carro.montadora = "Volksvagem";
+	    }
+
+}
